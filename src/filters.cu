86b9d26e5fd3dff94e0dddd85159224d2d3ee249
ysl1016 <154742313+ysl1016@users.noreply.github.com>
2025-01-27 13:42:07 +0900

    Fix: Update Sobel filter implementation for correct edge detection

--- src/filters.cu
+++ src/filters.cu
@@ -8,22 +8,22 @@ __global__ void sobelFilter(unsigned char* input, unsigned char* output, int wid
     if (x < width && y < height) {
         // Skip border pixels
         if (x == 0 || x == width-1 || y == 0 || y == height-1) {
-            output[y * width + x] = 0;
+            output[y * width + x] = input[y * width + x];
             return;
         }
 
         // Sobel kernels
-        int Gx = -input[(y-1)*width + (x-1)] + input[(y-1)*width + (x+1)]
-                 -2*input[y*width + (x-1)] + 2*input[y*width + (x+1)]
-                 -input[(y+1)*width + (x-1)] + input[(y+1)*width + (x+1)];
+        int Gx = input[(y-1)*width + (x+1)] + 2*input[y*width + (x+1)] + input[(y+1)*width + (x+1)]
+                 - input[(y-1)*width + (x-1)] - 2*input[y*width + (x-1)] - input[(y+1)*width + (x-1)];
 
-        int Gy = -input[(y-1)*width + (x-1)] - 2*input[(y-1)*width + x] - input[(y-1)*width + (x+1)]
-                 +input[(y+1)*width + (x-1)] + 2*input[(y+1)*width + x] + input[(y+1)*width + (x+1)];
+        int Gy = input[(y+1)*width + (x-1)] + 2*input[(y+1)*width + x] + input[(y+1)*width + (x+1)]
+                 - input[(y-1)*width + (x-1)] - 2*input[(y-1)*width + x] - input[(y-1)*width + (x+1)];
                  
         float val = sqrtf((float)(Gx*Gx + Gy*Gy));
         output[y * width + x] = (unsigned char)(val > 255 ? 255 : val);
     }
 }
+
 
 __global__ void gaussianBlur(unsigned char* input, unsigned char* output, int width, int height) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
