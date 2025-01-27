0278ccb30e2cc3ee46986324f7c529f060e31fea
ysl1016 <154742313+ysl1016@users.noreply.github.com>
2025-01-27 17:29:11 +0900

    Add error message: filters.cuh

--- include/filters.cuh
+++ include/filters.cuh
@@ -9,10 +9,16 @@ enum class FilterType {
 };
 
 // CUDA kernel declarations
-__global__ void sobelFilter(unsigned char* input, unsigned char* output, int width, int height);
-__global__ void gaussianBlur(unsigned char* input, unsigned char* output, int width, int height);
-__global__ void sharpenFilter(unsigned char* input, unsigned char* output, int width, int height);
+__global__ void sobelFilter(unsigned char* input, unsigned char* output, 
+                          int width, int height, int num_images);
+__global__ void gaussianBlur(unsigned char* input, unsigned char* output, 
+                           int width, int height, int num_images);
+__global__ void sharpenFilter(unsigned char* input, unsigned char* output, 
+                            int width, int height, int num_images);
 
 // Host functions
 void applyFilter(unsigned char* d_input, unsigned char* d_output, 
-                int width, int height, FilterType filter_type);
+                int width, int height, int num_images, FilterType filter_type);
+
+// Error checking function
+void checkCudaError(const char* msg);
